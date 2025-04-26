98739e0055d945b2761b2485103328aa5a1544d6
X <43752385+alizzo@users.noreply.github.com>
2025-04-26 10:36:35 -0400

    added files

--- src/snakeGame.java
+++ src/snakeGame.java
@@ -1,5 +1,5 @@
-public class snakeGame {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
     }
 }
